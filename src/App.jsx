d41d4f5632adda1877ba443451de10979dea5b88
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-08 03:00:15 -0700

    add font, start header

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,43 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import { createTheme, ThemeProvider, } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Roboto", 'sans-serif'
+    ].join(","),
+    h1: {
+      fontFamily: 'Playfair Display, serif',
+    },
+    h2: {
+      fontFamily: 'Playfair Display, serif',
+    },
+    h3: {
+      fontFamily: 'Playfair Display, serif',
+    },
+    h4: {
+      fontFamily: 'Playfair Display, serif',
+    },
+    h5: {
+      fontFamily: 'Playfair Display, serif',
+    },
+    h6: {
+      fontFamily: 'Playfair Display, serif',
+    },
+  }
+})
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <About />
       <Projects />
       <Contact />
       <Footer />
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Headshot from '../assets/images/pexels-anna-shvets-3727474.jpg'
+import { Box, Typography, CardMedia } from '@mui/material'
 
 function Header() {
     return (
-        <div>Header</div>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: "column",
+            alignItems: 'center',
+            padding: 2
+        }}>
+            <Box component="img" src={Headshot} sx={{ width: "100%" }}></Box>
+            <Typography variant="h2">Ashley Williams</Typography>
+        </Box>
     )
 }
 
