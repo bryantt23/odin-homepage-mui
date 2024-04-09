81e9d344fae8bbac654d762cc4df757f5aed6cd5
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-09 05:39:31 -0700

    add projects section

---
+++ src/components/Project.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Box, Typography, Grid, Card, Avatar } from '@mui/material'
+import Github from '../assets/images/github-mark.png'
+import LaunchIcon from '@mui/icons-material/Launch';
+
+function Project({ color }) {
+    return (
+        <Grid item xs={12} sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Card variant="elevation" sx={{ margin: 1 }}>
+                <Box sx={{ backgroundColor: color, height: 300, alignContent: 'center', textAlign: 'center' }}>
+                    <Typography variant="h4" sx={{ color: 'white' }}>screenshot of project</Typography>
+                </Box>
+                <Box sx={{ height: 200, padding: 2 }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Typography variant="h5" sx={{ paddingLeft: 1 }}>Project name</Typography>
+                        <Box sx={{ display: 'flex', gap: 2 }}>
+                            <Box component="img"
+                                src={Github}
+                                sx={{
+                                    height: 30
+                                }}>
+                            </Box>
+                            <LaunchIcon
+                                sx={{
+                                    fontSize: 30
+                                }} />
+                        </Box>
+                    </Box>
+                    <Typography sx={{ padding: 1 }}>
+                        Short description of the project. Just a couple sentences will do.
+                    </Typography>
+                </Box>
+            </Card>
+        </Grid>
+    )
+}
+
+export default Project
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Box, Typography, Grid } from '@mui/material'
+import Project from './Project'
 
 function Projects() {
+    const projects = [
+        { id: 1, color: "#7c75ca" },
+        { id: 2, color: "#d88f39" },
+        { id: 3, color: "#5e8f4d" },
+        { id: 4, color: "#ca7599" },
+        { id: 5, color: "#4592d9" },
+        { id: 6, color: "#5e8f4d " },
+    ]
     return (
-        <div>Projects</div>
+        <Box sx={{ padding: 2, }}>
+            <Box>
+                <Typography variant="h4" sx={{ textAlign: 'center', paddingBottom: 1 }}>Projects</Typography>
+            </Box>
+            <Grid container>
+                {projects.map(project => <Project key={project.id} color={project.color} />)}
+            </Grid>
+
+
+        </Box>
     )
 }
 
