acc0ad1747642b29f167ca687f555c0a515a0bce
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-25 00:44:46 -0700

    set footer image using height auto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Headshot from '../assets/images/pexels-anna-shvets-3727464.jpg'
+
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <Box component="img"
+            src={Headshot}
+            sx={{
+                width: "100%",
+                height: "auto"
+            }}>
+        </Box>
     )
 }
 
