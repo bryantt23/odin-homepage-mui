fc6abb4577320ec47312c39096097dfe2a39c841
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-28 02:58:00 -0700

    remove about and move into Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Headshot from '../assets/images/pexels-anna-shvets-3727474.jpg'
-import { Box, Typography, Grid } from '@mui/material'
-import About from './About'
+import { Box, Typography, Grid, Avatar, Hidden } from '@mui/material'
+import Github from '../assets/images/github-mark.png'
+import LinkedIn from '../assets/images/LinkedIn_icon_circle.png'
+import Twitter from '../assets/images/twitter-3.svg'
 
 function Header() {
     return (
@@ -43,7 +45,24 @@ function Header() {
                     </Box>
                 </Box></Grid>
             <Grid item xs={12} md={6}>
-                <About />
+                <Box sx={{ padding: 2 }}>
+                    <Box sx={{ padding: 2, backgroundColor: 'white' }}>
+                        <Typography variant="h4" sx={{ textAlign: 'center', paddingBottom: 1 }}>About me</Typography>
+                        <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Typography>
+                    </Box>
+                    <Hidden smUp>
+                        <Box sx={{
+                            display: 'flex',
+                            justifyContent: "center",
+                            padding: 1,
+                            gap: 2
+                        }}>
+                            <Avatar src={Github} />
+                            <Avatar src={LinkedIn} />
+                            <Avatar src={Twitter} />
+                        </Box>
+                    </Hidden>
+                </Box>
             </Grid>
         </Grid>
     )
