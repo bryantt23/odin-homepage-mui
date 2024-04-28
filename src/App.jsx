368d60f183659ed8e0988300aa04080cd1e37487
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-28 02:17:40 -0700

    footer for tablet

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
-import Contact from './components/Contact'
 import Footer from './components/Footer'
 import { createTheme, ThemeProvider, } from '@mui/material';
 
@@ -38,7 +37,6 @@ function App() {
       <Header />
       <About />
       <Projects />
-      <Contact />
       <Footer />
     </ThemeProvider>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import Headshot from '../assets/images/pexels-anna-shvets-3727464.jpg'
-
+import Contact from './Contact'
 
 function Footer() {
     return (
-        <Box component="img"
-            src={Headshot}
-            sx={{
-                width: "100%",
-                height: "auto"
-            }}>
-        </Box>
+        <Grid container sx={{ backgroundColor: "#0791b2" }}>
+            <Grid item md={6} xs={12}>
+                <Contact />
+            </Grid>
+            <Grid item md={6} xs={12}>
+                <Box component="img"
+                    src={Headshot}
+                    sx={{
+                        objectFit: "contain",
+                        width: "100%",
+                        height: "100%"
+                    }}>
+                </Box></Grid>
+        </Grid>
     )
 }
 
